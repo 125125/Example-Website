982e9102d36c02e249b2831c0e6f5b40c6fb2a7a
tok124 <tim-levinsson@hotmail.com>
2017-05-04 14:58:58 +0200

    Added readme

--- includes/nav.php
+++ includes/nav.php
@@ -41,7 +41,7 @@
         }
         ?>
         <li class='nav-item'>
-          <a class='nav-link' href='forum'>Forum</a>
+          <a class='nav-link disabled' href='#'>Forum</a>
         </li>
       </ul>
     </div>
